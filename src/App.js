2fa89506e25ae8b3a60c65b8bbae31599e414b3b
Aryan Rai <aryanx23@pop-os.localdomain>
2022-11-25 16:55:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Sector1 from './Sectors/sector1';
+import Dashboard from './Dashboard/dashBoard';
+import Home from './Home/home';
+import SignIn from './SignIn/signIn';
+import React from 'react';
+import { Routes, Route, Navigate} from "react-router-dom";
+import PrivateRoutes from './utils/privateRoutes.js'; 
+import axios from 'axios'; 
+function App() {
+  React.useEffect(()=>{axios({
+    method: 'post',
+    url:"http://localhost:4000/login",
+    headers: {'Content-Type': 'application/json'}, 
+    withCredentials:true
+    }).then(response=>{
+        localStorage.setItem("isLoggedIn",JSON.stringify(response.data));
+    });
+  },[]);
+  return (
+      <div className='App'>
+          <Routes>
+            <Route exact path="/sonicLibrarySystem/" element={ <Home/> } />
+            <Route exact path="/sonicLibrarySystem/signIn" element={ <SignIn/> } />
+            <Route path="*" element={<Navigate to="/sonicLibrarySystem/" replace />}/>
+            <Route  element={<PrivateRoutes/>} >
+              <Route exact path="/sonicLibrarySystem/dashboard" element={ <Dashboard/> } />
+              <Route exact path="/sonicLibrarySystem/sector1" element={ <Sector1/> } />
+            </Route>
+          </Routes>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar/navBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import './navBar.css';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import axios from "axios";
+export default function Navbar(){
+    const navigate = useNavigate();
+    const [dataVisi,setDataVisi]=React.useState("false");
+    function handleClick(){
+        if(dataVisi==="false")
+            setDataVisi(prev=>"true");
+        else
+            setDataVisi(prev=>"false");
+    }
+    function handleLogOut(){
+        axios({
+            method: 'post',
+            url:"http://localhost:4000/logout",
+            headers: {'Content-Type': 'application/json'}, 
+            withCredentials:true
+        }).then(response=>{
+            localStorage.setItem("isLoggedIn",JSON.stringify(response.data));
+            navigate("/sonicLibrarySystem/");
+        });
+    }
+    return(
+        <div className="navBody">
+            <motion.div className="navLeft" initial={{ x: '-100vw',opacity:0}} animate={{x:'0vw',opacity:1}} transition={{type:'spring', duration: 1.5, bounce:0}}>
+                <span>Sonic Library System</span></motion.div>
+            <motion.div className="navRight" initial={{ x: '+100vw',opacity:0}} animate={{x:'0vw',opacity:1}} transition={{type:'spring', duration: 1.5, bounce:0}}>
+                <img src={process.env.PUBLIC_URL+"/images/userIcon.png"} alt="userIcon" />
+                <span>Administrator</span>
+                <img src={process.env.PUBLIC_URL+"/images/menuIcon.png"} onClick={handleClick} className="menuIcon" alt="menuIcon" />
+                <div className="navPane"  data-visibility={dataVisi} >
+                    <div className="navPaneTitle"><img src={process.env.PUBLIC_URL+"/images/closeIcon.png"} className="closeIcon" onClick={handleClick} alt="" /></div>
+                    <div className="navPaneBody">
+                        <div className="imageBg">
+                            <img src={process.env.PUBLIC_URL+"/images/userIconBlack.png"} className="userIconBlack" alt="blackUser" />
+                        </div>
+                        <span className="navBodyText">Administrator</span>
+                    </div>
+                    <button className="navPaneButton" onClick={handleLogOut}>Logout</button>
+                </div>
+            </motion.div>
+        </div>
+    );
+}
